aa2e3788494dcca350cf28f5fe4b38a49c5357e4
sajed <liningset2025@gmail.com>
2026-08-12 23:32:32 +0330

    renamed index.js to index.jsx for emmet auto completion purposes, also added a README.md and wrote some simple react components as exercise

--- index.jsx
+++ index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = React.createElement("h1", {}, "Hello again Sajed.");
+function HeadingComponent() {
+  return (
+    <div id="header">
+      <h1>Hello From inside the component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum saepe
+        amet ipsum cumque distinctio voluptates nostrum quidem quas impedit ab
+        voluptatum possimus, hic vero? Quod hic iste similique ad ipsam.
+      </p>
+    </div>
+  );
+}
+
+function SomethingInIt() {
+  return <span>this is just a test to see where it lands</span>;
+}
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(element);
+root.render(<HeadingComponent>some text</HeadingComponent>);
