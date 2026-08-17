870893e43dade8404976eca4ac7851026d54b297
sajed <liningset2025@gmail.com>
2026-08-17 22:26:12 +0330

    added search restaurants feature via useState + useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { PUBLIC_DIR } from "../utils/constants";
 import NavList from "./NavList";
 import { Link } from "react-router";
 
-const Header = () => {
+const Header = ({ setFilterByString }) => {
   /*
     Dunno why but logo.png is not being loaded currently
   
@@ -16,6 +16,10 @@ const Header = () => {
           <img src={PUBLIC_DIR + "logo.png"} alt="Logo" />
         </Link>
       </nav>
+<<<<<<< HEAD
+=======
+      <Hero setFilterByString={setFilterByString} />
+>>>>>>> 8aba49b (added search restaurants feature via useState + useEffect)
     </header>
   );
 };
