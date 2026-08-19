2dbf40e4df4b6eaaad3ec95c57701d2e12d46154
sajed <liningset2025@gmail.com>
2026-08-19 20:59:44 +0330

    added fallback shimmer UI for dynamically fetched data

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,12 @@
+const Shimmer = () => {
+  const iterate15times = () => {
+    const array = [];
+    for (let i = 0; i <= 15; i++) {
+      array.push(<div className="shimmer-card" key={i}></div>);
+    }
+    return array;
+  };
+  return <ul className="shimmer-cards">{iterate15times()}</ul>;
+};
+
+export default Shimmer;
