9e7d56337211f767608a4381be81b81d0c82a079
sajed <liningset2025@gmail.com>
2026-08-19 20:33:26 +0330

    changed the app's language to farsi and changed the logo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
 import data from "../utils/data.json";
 import RestaurantCard from "./RestaurantCard";
+import { PROXY, API } from "../utils/constants";
 
 const Main = ({ filterByString }) => {
   const [restaurants, setRestaurants] = useState(data);
+
+  const fetchData = async (page = 1) => {
+    const response = await fetch(API.concat(`&page=${page}&page_size=10`));
+    const data = await response.json();
+    data.data.finalResult.shift();
+    setRestaurants(data.data.finalResult);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const filtered = restaurants.filter((res) => {
       return (
         new RegExp(filterByString, "i").test(res.title) ||
-        res.cuisines.some((cuisine) =>
-          cuisine.toLowerCase().includes(filterByString),
+        res?.cuisinesArray.some((cuisine) =>
+          cuisine.title.toLowerCase().includes(filterByString),
         )
       );
     });
@@ -19,12 +31,16 @@ const Main = ({ filterByString }) => {
 
   return (
     <main id="explore">
-      <h2>Restaurants</h2>
+      <h2>رستوران ها</h2>
       <ul className="restaurants">
-        {restaurants.map((restaurant) => (
-          <RestaurantCard data={restaurant} key={restaurant.id} />
+        {restaurants.map((restaurant, i) => (
+          <RestaurantCard
+            data={restaurant?.data}
+            key={restaurant?.id ? restaurant.id : i}
+          />
         ))}
       </ul>
+      <button onClick={() => fetchData(2)}>fetch more</button>
     </main>
   );
 };
