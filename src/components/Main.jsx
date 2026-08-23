15989ce04b1c1fc41522444543145cb31b141b31
sajed <liningset2025@gmail.com>
2026-08-23 15:44:39 +0330

    fixed the bug where restaurants data was being mutated after filtering + tweaked some minor styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,38 +6,48 @@ import Shimmer from "./Shimmer";
 
 const Main = ({ filterByString }) => {
   const [restaurants, setRestaurants] = useState([]);
+  const [restaurantsFiltered, setRestaurantsFiltered] = useState(restaurants);
 
   const fetchData = async (page = 1) => {
-    const response = await fetch(API.concat(`&page=${page}&page_size=10`));
+    const response = await fetch(API.concat(`&page=${page}&page_size=16`));
     const data = await response.json();
+    //removing unnecessary text entry
     data.data.finalResult.shift();
     setRestaurants(data.data.finalResult);
+    //i explained below why i didn't just set both states here
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  /* i populated it seperately because i dont want possible pagination which
+  will use the same fetchData function call to ruin the filtered list of restaurants */
+  useEffect(() => {
+    setRestaurantsFiltered(restaurants);
+  }, [restaurants]);
+
   useEffect(() => {
     const filtered = restaurants.filter((res) => {
+      const data = res.data;
       return (
-        new RegExp(filterByString, "i").test(res.title) ||
-        res?.cuisinesArray.some((cuisine) =>
+        new RegExp(filterByString, "i").test(data.title) ||
+        data?.cuisinesArray.some((cuisine) =>
           cuisine.title.toLowerCase().includes(filterByString),
         )
       );
     });
-    setRestaurants(filtered);
+    setRestaurantsFiltered(filtered);
   }, [filterByString]);
 
   return (
     <main id="explore">
       <h2>رستوران ها</h2>
-      {restaurants.length === 0 ? (
+      {restaurantsFiltered.length === 0 ? (
         <Shimmer />
       ) : (
         <ul className="restaurants">
-          {restaurants.map((restaurant, i) => (
+          {restaurantsFiltered.map((restaurant, i) => (
             <RestaurantCard
               data={restaurant?.data}
               key={restaurant?.id ? restaurant.id : i}
