235ed5b3854beb23c7d1d32f87199f728736666a
sajed <liningset2025@gmail.com>
2026-08-24 16:36:12 +0330

    made routing proper with children routes instead of adding in recurring components manually

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,4 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
 const AboutPage = () => {
-  return (
-    <>
-      <Header />
-      <h1>About Us</h1>
-      <Footer />
-    </>
-  );
+  return <h1>About Us</h1>;
 };
 export default AboutPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import Header from "../components/Header";
 import Main from "../components/Main";
-import Footer from "../components/Footer";
 
 const HomePage = () => {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+  return <Main />;
 };
 
 export default HomePage;
