c774aed2d3e702dee957a6c97802ea7b38116a04
sajed <liningset2025@gmail.com>
2026-08-31 19:58:05 +0330

    finished up aboutpage with dummy data of team members

--- src/components/AboutTeamMember.jsx
+++ src/components/AboutTeamMember.jsx
@@ -6,9 +6,24 @@ class AboutTeamMember extends React.Component {
   }
 
   render() {
-    const { name } = this.props;
+    const { name, cover } = this.props.data;
 
-    return <h2>team member {name}</h2>;
+    return (
+      <li className="member">
+        <div className="cover">
+          <img src={cover} alt="photo" />
+        </div>
+        <div className="info">
+          <h2>{name}</h2>
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+            کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+          </p>
+        </div>
+      </li>
+    );
   }
 }
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,25 +7,44 @@ class AboutPage extends Component {
     this.state = {
       staffMembers: [],
     };
-    this.timeout = () => {
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
       console.log("timeout ran");
 
       this.setState({
         staffMembers: [
-          { name: "ali" },
-          { name: "mehdi" },
-          { name: "karim" },
-          { name: "gholam" },
+          {
+            id: 1,
+            name: "علی قلیزاده",
+            cover:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk4S_WSz7FM9B_XMWTn4MoxEHcwTs3jlc6E7Yjf7cdRxDIw3DJeR9_AKM8&s=10",
+          },
+          {
+            id: 2,
+            name: "مهسا خدایار",
+            cover:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6SUJf1dTNXq5OzsqylWW_O3tL2uCTKOyOKOH9w4pCrVSAuy8Rz9g3F3E&s=10",
+          },
+          {
+            id: 3,
+            name: "موسی آقایی",
+            cover:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV5oOLCUkWWUyMtoQxOhOBIT-_tffkloGbwAw-FRwJCL_8TekBCyPf41qc&s=10",
+          },
+          {
+            id: 4,
+            name: "رحیم رحمتی",
+            cover:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD9Uztwcgv1nbcEgAwbz3Ha3CjGxKh-c_cZV6JhGLdN1Xrt1oqdJ1s090&s=10",
+          },
         ],
       });
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(this.timeout, 2000);
+    }, 2000);
   }
   componentWillUnmount() {
-    clearTimeout(this.timeout);
+    clearInterval(this.timeout);
   }
 
   render() {
@@ -34,8 +53,8 @@ class AboutPage extends Component {
         <div className="aboutpage-wrapper">
           <h1>ما که هستیم؟</h1>
           <ul className="staff-members">
-            {this.state.staffMembers.map((member, i) => (
-              <AboutTeamMember key={i} name={member.name} />
+            {this.state.staffMembers.map((member) => (
+              <AboutTeamMember key={member.id} data={member} />
             ))}
           </ul>
         </div>
